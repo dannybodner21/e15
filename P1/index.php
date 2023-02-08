7da3d6396f0fb416518d3e1961e76738dfa48064
Daniel Bodner <dab7745@g.harvard.edu>
2023-02-08 01:50:54 +0000

    Completed logic and added comments.

--- P1/index.php
+++ P1/index.php
@@ -1,17 +1,25 @@
 <?php
 
+// PROJECT 1:
+
+// #1 IS PALINDROME:
+
+// Get input string
 $input = 'racecaroiae!';
+// Set the regular expression pattern
 $pattern = '/[^a-z]/';
+// Turn string into lowercase and remove non-alphabet characters
 $input = strtolower(preg_replace($pattern, '', $input));
-
-// Check palindrome
+// Reverse the string
 $reverse = strrev($input);
-
+// Check palindrome and show result
 if ($reverse == $input) {
-    echo("they the same!");
+    echo("Yes, your string is a palindrome.");
 } else {
-    echo("they do be different...");
+    echo("Sorry, your string is NOT a palindrome.");
 }
+
+// #2 VOWEL COUNT:
 
 // Count vowels (aeiou)
 $aCount = substr_count($input,'a');
@@ -19,12 +27,15 @@ $eCount = substr_count($input,'e');
 $iCount = substr_count($input,'i');
 $oCount = substr_count($input,'o');
 $uCount = substr_count($input,'u');
+// Get the total number of vowels
 $totalVowels = $aCount + $eCount + $iCount + $oCount + $uCount;
-
+// Show results
 echo($totalVowels);
 
-// Letter shift
-// String to shift
+
+// #3 LETTER SHIFT:
+
+// Get the string to shift
 $test = 'abc';
 // Turn string into array of characters
 $test = str_split($test);
@@ -39,10 +50,27 @@ $alphabet = [
     'g' => 7,
     'h' => 8,
     'i' => 9,
-    'j' => 10
+    'j' => 10,
+    'k' => 11,
+    'l' => 12,
+    'm' => 13,
+    'n' => 14,
+    'o' => 15,
+    'p' => 16,
+    'q' => 17,
+    'r' => 18,
+    's' => 19,
+    't' => 20,
+    'u' => 21,
+    'v' => 22,
+    'w' => 23,
+    'x' => 24,
+    'y' => 25,
+    'z' => 26
 ];
 
-// Function to take an array of string chars and shift it
+// Function to take an array of string characters and shift it a specific amount
+// Returns new array of shifted characters
 function shiftLetters($alphabet, $myArray, $shiftSize) {
 
     // Loop through the char array
@@ -65,13 +93,23 @@ function shiftLetters($alphabet, $myArray, $shiftSize) {
     return $shiftedArray;
 }
 
-//$myNumber = $alphabet[$test[2]];
-//$myNumber++;
-//$result = array_search($myNumber, $alphabet);
-
-var_dump($test);
+// Do the shift
 $result = shiftLetters($alphabet, $test, 1);
+// Show results
 var_dump($result);
+
+
+// #4 CAESAR SHIFT - (move the letters over a specified number of characters):
+
+// Get number to shift characters
+$numberToShift = 3;
+// String to shift -> into array of characters
+$testTwo = str_split('abc');
+// Do the shift
+$resultTwo = shiftLetters($alphabet, $testTwo, $numberToShift);
+// Show results
+var_dump($resultTwo);
+
 
 
 require  'index-view.php';
